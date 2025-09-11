61667310a2d87a754f40f5c38d952ba43212e0be
BADABAMDOOG <sbya1228@gmail.com>
2025-09-11 10:55:50 +0900

    [init]최초 커밋
    수업을 위한 최초 커밋

---
+++ HelloGIt.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ConsoleGit
+{
+
+    class HelloGit
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("HelloGit");
+            //이렇게 적고 저장까지!
+        
+        }
+
+    
+    
+    }
+
+
+
+
+}
